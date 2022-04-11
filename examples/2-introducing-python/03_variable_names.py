62eab82bfb64d7aa29ef409bcc8d6c4baae618c0
Ian Johnston <reaper@obairlann.net>
2022-04-11 14:11:02 -0700

    initial commit

---
+++ examples/2-introducing-python/03_variable_names.py
@@ -0,0 +1,18 @@
+# Variable name examples
+
+# Too short
+u = 'Ian Johnston'
+
+# Too verbose
+current_user_first_last = 'Ian Johnston'
+
+# CamelCase
+userName = 'Ian Johnston'
+
+# Leading capital letter - this should only be used for classes
+Username = 'Ian Johnston'
+
+# Good variable name
+user_name = 'Ian Johnston'
+
+# Bonus question: Why might I choose user_name instead of username here?
